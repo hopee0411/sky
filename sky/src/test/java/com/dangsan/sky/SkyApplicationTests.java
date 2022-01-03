db72870da0e7072ec60c30a5f2be656e9fe5222a
hopee0411 <84006813+hopee0411@users.noreply.github.com>
2022-01-03 11:03:37 +0900

    first

---
+++ sky/src/test/java/com/dangsan/sky/SkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dangsan.sky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
